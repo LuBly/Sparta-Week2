01c591b0528b1f4dd5833a785ba3c04d3ab93795
Park Hyunho <lumenize@ajou.ac.kr>
2024-04-23 16:51:41 +0900

    study Ch4

---
+++ Ch4/Ch4/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Ch4
+{
+    internal class Program
+    {
+        // 인터페이스 구현해보기
+
+        public interface IMovable
+        {
+            void Move(int x, int y);
+        }
+
+        public class Player : IMovable 
+        { 
+            public void Move(int x, int y)
+            {
+
+            }
+        }
+
+        public class Enemy : IMovable
+        {
+            public void Move(int x, int y)
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Ch4/IUsable/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace IUsable
+{
+    internal class Program
+    {
+        public interface IUsable
+        {
+            void Use();
+        }
+
+        // 아이템 클래스
+        public class Item : IUsable
+        {
+            public string Name { get; set; }
+
+            public void Use()
+            {
+                Console.WriteLine("아이템 {0}을 사용했습니다.", Name);
+            }
+        }
+
+        // 플레이어 클래스
+        public class Player
+        {
+            public void UseItem(IUsable item)
+            {
+                item.Use();
+            }
+        }
+
+        // 게임 실행
+        static void Main()
+        {
+            Player player = new Player();
+            Item item = new Item { Name = "Health Potion" };
+            player.UseItem(item);
+        }
+    }
+}
